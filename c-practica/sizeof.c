9156e30d544b18c226bf5121c3a63ecaf2116d3a
Jonatha32 <jonaperez038@gmail.com>
2024-06-09 23:03:38 +0000

    sizeof.c

---
+++ c-practica/sizeof.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int miInt;
+float miFloat;
+double miDouble;
+char miChar;
+
+printf("%lu\n", sizeof(miInt));
+printf("%lu\n", sizeof(miFloat));
+printf("%lu\n", sizeof(miDouble));
+printf("%lu\n", sizeof(miChar));
+return 0;
+}

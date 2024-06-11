d5cacf7c1a6b57d38a3508e53170ce8e4d2b5217
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 15:45:27 +0000

    dowhile

---
+++ c-practica/dowhile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+  int uno = 1;
+
+  while (uno <= 20) {
+    if (uno < 20) {
+      printf("Holaa\n");
+    } else {
+      printf("Chau!\n");
+    }
+    uno = uno + 1;
+  }
+}

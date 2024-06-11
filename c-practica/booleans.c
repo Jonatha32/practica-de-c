704724b7e1d9aeb734cb5119dae4a8e5b47bfcb4
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 12:51:11 +0000

    booleans

---
+++ c-practica/booleans.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+  int x = 44;
+  int y = 67;
+  printf("%d\n", x <  y); /*Le decimos que imprima "1" si x es menor a y, si es falso se imprime "0"*/
+  return 0;
+}
+	  

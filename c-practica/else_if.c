5f3518459f5ecee381eee494e21e04c68e2f55be
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 13:41:55 +0000

    else_if

--- c-practica/else_if.c
+++ c-practica/else_if.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main() {
-  int x = 66;
+  int x = 66; /*Definimos dos variables*/
   int y = 68;
 
-  if (x == y) {
+  if (x == y) { /*Si x es igual a y, 
+		  que imprima lo siguiente*/
     printf("uh...\n");
-  }else if (x > y) {
+  }else if (x > y) { /*Si la primer condicion no se cumple, 
+		       se ejecuta la siguiente. Si x es mayor a y,
+		       se imprime lo otro*/
     printf("fua...\n");
   } else {
-    printf("jajaja\n");
+    printf("jajaja\n");  /*Si ninguna condicion se cumple, 
+			   se imprime "jajajaja"*/
   }
 }

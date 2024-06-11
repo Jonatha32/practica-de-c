ae0005741177f594ca8f2ab6465f2dcfc6486908
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 16:50:28 +0000

    nested_loops

---
+++ c-practica/nested_loops.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+  int x, y;
+
+  for (x = 1; x <= 2; ++x) {
+    printf("JAJAJA: %d\n", x);
+
+    for (y = 1; y <= 3; ++y) {
+      printf("Clave: %d\n", y);
+    }
+  }
+}
+			      

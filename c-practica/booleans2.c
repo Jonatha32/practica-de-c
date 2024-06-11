36041d9b6b5f032393d0197a86e122d00b2adbc7
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 13:01:39 +0000

    booleans2

---
+++ c-practica/booleans2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdbool.h>
+int main() {
+  int good = 43;
+  int bad = 32;
+  int xd = 312;
+
+  printf("%d\n", good > bad);
+  printf("%d\n", bad < xd);
+  return 0;
+}
+  

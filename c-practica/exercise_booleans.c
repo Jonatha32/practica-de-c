5e31ccc485858e37484e920042fdb953b2ea415e
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 13:10:42 +0000

    exercise_booleans.c

---
+++ c-practica/exercise_booleans.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+int myAge = 17;
+int VotingAge = 18;
+
+ if (myAge >= VotingAge) {
+   printf("Puedes Votar!\n");
+ } else {
+   printf("No puede Votar :(\n");
+ }
+return 0;
+}

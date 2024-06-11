5a0406d080ad6e2e977a3a09fb558bd33f29f43d
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 13:38:11 +0000

    else_if

---
+++ c-practica/else_if.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+  int x = 66;
+  int y = 68;
+
+  if (x == y) {
+    printf("uh...\n");
+  }else if (x > y) {
+    printf("fua...\n");
+  } else {
+    printf("jajaja\n");
+  }
+}

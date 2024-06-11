f2888bdc150b886fa600b783e71ae576451c040f
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 15:24:04 +0000

    do_while_exercise

---
+++ c-practica/do_while_exercise.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+  int CuentaAtras = 10;
+
+  while (CuentaAtras > 0) {
+    printf("%d\n", CuentaAtras);
+    CuentaAtras--;
+  }
+
+  printf("Hola\n");
+  return 0;
+}

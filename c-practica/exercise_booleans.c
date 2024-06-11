06f1039ed735bd8600f5bf9a454aceb682eb7394
Jonatha32 <jonaperez038@gmail.com>
2024-06-11 13:18:14 +0000

    exercise_booleans

--- c-practica/exercise_booleans.c
+++ c-practica/exercise_booleans.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main() {
-int myAge = 17;
+  int myAge = 17; /*Definimos dos variables "Mi Edad" y la "Edad de Votacion"*/
 int VotingAge = 18;
 
- if (myAge >= VotingAge) {
+ if (myAge >= VotingAge) { /*Le decimos que si "Mi Edad" es mayor o 
+			     igual a la "Edad de Votacion" imprima un mensaje*/
    printf("Puedes Votar!\n");
  } else {
-   printf("No puede Votar :(\n");
+   printf("No puede Votar :(\n"); /*Si "Mi Edad" es menor a la "Edad 
+				    de Votacion" se imprime el siguiente mensaje*/
  }
 return 0;
 }

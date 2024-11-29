771aab525cd10f071ed8ac50fbd4035bf2a6a630
Jonathan Pérez <135724908+Jonatha32@users.noreply.github.com>
2024-11-29 10:03:20 -0300

    Delete c-practica directory

--- c-practica/charASCII.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-  char a = 43, b = 23, c = 47;
-  printf("%c\n", a);
-  printf("%c\n", b);
-  printf("%c\n", c);
-  return 0;
-}
--- c-practica/hello_world.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-  printf("HELLO_WORLD INTER DE CANELONES\n\n"); /* ESTE ES EL MEJOR EQUIPO
-						   DEL MUNDO */
-  printf("MUJICA ES EL MAS GRANDE,"); // Esto es una opinion jajaja
-  printf("PERO NOS ROBO EL PAIS\n");
-    return 0;
-
-}
